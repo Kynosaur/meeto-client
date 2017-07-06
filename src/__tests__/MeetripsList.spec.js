b665169aa5f876d36ab9e473d0e11be8a54524c8
Prabu Sathan <prabu.sathan@gmail.com>
2017-07-06 15:37:45 +0100

    Updates MeetripsList to fetch data from api and render the list of Meetrips

--- src/__tests__/MeetripsList.spec.js
+++ src/__tests__/MeetripsList.spec.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import MeetripsList from '../components/MeetripsList';
+import { sleep } from '../testHelpers';
+import { XMLHttpRequest } from 'xmlhttprequest';
+global.XMLHttpRequest = XMLHttpRequest;
 
 describe('MeetripsList', () => {
   it('Displays all the meetrips;', () => {
     const meetripsList = shallow(<MeetripsList />);
     expect(meetripsList.find('h2').text()).toEqual('List of Meetrips');
+  })
+
+  it('Fetches the meetrips', async () => {
+    const meetripsList = mount(<MeetripsList />);
+    await sleep(500);
+    expect(meetripsList.find('#meetrip1').text()).toEqual('manchesterlondon12345');
   })
 })
