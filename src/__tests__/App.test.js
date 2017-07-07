4c6c79337a438afe16f887da7926c40ae10969e1
jade <jadealvares@hotmail.co.uk>
2017-07-07 15:13:10 +0100

    (WIP) to create new meetrip

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { shallow, mount, render } from 'enzyme';
 import { expect } from 'chai';
 import App from '../components/App';
+import NewMeetripForm from '../components/NewMeetripForm';
+import { sleep } from '../testHelpers';
 
 import { XMLHttpRequest } from 'xmlhttprequest';
 global.XMLHttpRequest = XMLHttpRequest;
@@ -14,8 +16,24 @@ describe('App', () => {
     ReactDOM.render(<App />, div);
   });
 
-  it('Displays all the meetrips;', () => {
+  it('Displays all the meetrips', () => {
     const appWrapper = mount(<App />);
     expect(appWrapper.find('h1').exists()).to.equal(true);
+  })
+
+  it('creates a new meetrip', async () => {
+    const appWrapper = mount(<App />);
+    const formWrapper = mount(<NewMeetripForm />);
+    const fromLocation = formWrapper.ref('fromLocation');
+    const toLocation = formWrapper.ref('toLocation');
+    const contactInfo = formWrapper.ref('contactInfo');
+    const submitButton = formWrapper.ref('submitMeetripButton');
+    fromLocation.simulate('change', { target: { value: 'London' } });
+    toLocation.simulate('change', { target: { value: 'Manchester' } });
+    contactInfo.simulate('change', { target: { value: '555 123' } });
+    submitButton.simulate('click');
+    appWrapper.update();
+    await sleep(500)
+    expect(appWrapper.text()).to.include('From: London, To: Manchester, Contact: 555 123');
   })
 });
--- src/components/NewMeetripForm.js
+++ src/components/NewMeetripForm.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
 class NewMeetripForm extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {from_location: '', to_location: '', contact_info: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.createMeetrip = this.createMeetrip.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({[name]: value})
+  }
+
+  createMeetrip(event) {
+    console.log(JSON.stringify(this.state));
+    event.preventDefault();
+    fetch('http://localhost:9292/meetrips/create', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+      body: JSON.stringify(this.state)
+    });
+  }
+
   render() {
     return (
       <form onSubmit={this.createMeetrip}>
-        <label htmlFor='from-location'>From:</label>
-        <input type='text' name='from-location' placeholder='E.g. London' />
-        <label htmlFor='to-location'>To:</label>
-        <input type='text' name='to-location' placeholder='E.g. Manchester' />
-        <label htmlFor='contact-info'>Contact Information:</label>
-        <input type='text' name='contact-info' placeholder='E.g. 555 0123' />
-        <input type='submit' value='Create Meetrip' />
+        <label htmlFor='from_location'>From:</label>
+        <input type='text' name='from_location' placeholder='E.g. London' ref='fromLocation' onChange={this.handleChange} />
+        <label htmlFor='to_location'>To:</label>
+        <input type='text' name='to_location' placeholder='E.g. Manchester' ref={'toLocation'} onChange={this.handleChange}/>
+        <label htmlFor='contact_info'>Contact Information:</label>
+        <input type='text' name='contact_info' placeholder='E.g. 555 0123' ref={'contactInfo'} onChange={this.handleChange}/>
+        <input type='submit' value='Create Meetrip' ref={'submitMeetripButton'} />
       </form>
     )
   }
